d397f57404b3d53782fdec6fa2e23d2966150d21
StasVenger <stasvenger1@gmail.com>
2024-03-12 01:40:16 +0600

    Added useEffect for Map with setview

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -32,6 +32,12 @@ function Map(props: MapProps): JSX.Element {
   const map = useMap({mapRef, city});
 
   useEffect(() => {
+    if (map && city) {
+      map.setView([city.location.latitude, city.location.longitude], city.location.zoom);
+    }
+  }, [map, city]);
+
+  useEffect(() => {
     if (map) {
       const markerLayer = layerGroup().addTo(map);
       offers.forEach((offer) => {
