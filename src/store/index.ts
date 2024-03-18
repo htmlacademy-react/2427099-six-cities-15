614434fc273b7a3c333ed42a63ce5ec91536dac7
StasVenger <stasvenger1@gmail.com>
2024-03-18 15:20:01 +0600

    Added redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { offersSlice } from './slices/offers';
+import { createAPI } from 'src/services/api';
+
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     [offersSlice.name]: offersSlice.reducer
   },
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare({
+      thunk: {
+        extraArgument: api
+      }
+    })
 });
