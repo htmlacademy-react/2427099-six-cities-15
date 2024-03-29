2cff95babeb1f8dbee175823a97c4b8169a218e8
StasVenger <stasvenger1@gmail.com>
2024-03-29 15:09:26 +0600

    Created tests for comment reducers.

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,5 +1,6 @@
-import { internet, name } from 'faker';
+import { internet, lorem, name } from 'faker';
 import { User } from '@type/user';
+import { Comment } from '@type/comment';
 
 export const makeFakeUser = (): User => ({
   name: name.title(),
@@ -7,4 +8,16 @@ export const makeFakeUser = (): User => ({
   isPro: false,
   email: internet.email(),
   token: 'secret'
+});
+
+export const makeFakeComment = (): Comment => ({
+  id: crypto.randomUUID(),
+  date: new Date().toISOString(),
+  user: {
+    name: name.title(),
+    avatarUrl: internet.avatar(),
+    isPro: false,
+  },
+  comment: lorem.text(),
+  rating: Math.floor(Math.random() * 5) + 1,
 });
